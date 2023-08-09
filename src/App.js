373ef7f3c70cfa3c9360b922714bb38241841d78
Stefan Vlad <72700839+StefanVlad0@users.noreply.github.com>
2023-08-09 12:13:26 +0300

    CDN added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +9,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div id="quote-box">
+          <p id="text">Text</p>
+          <p id="author">Author</p>
+          <button id="new-quote">New Quote</button>
+          <a id="tweet-quote" href="twitter.com/intent/tweet">
+            Tweet this
+          </a>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
